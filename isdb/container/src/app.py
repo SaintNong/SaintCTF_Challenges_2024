7ba9b82e3b88bced3d887c6abd550343f54d0eee
Alexolotal <Alexolotal@protonmail.com>
2024-06-27 18:24:32 +0800

    Made sqli challenge harder

--- isdb/container/src/app.py
+++ isdb/container/src/app.py
@@ -1,3 +1,4 @@
+from os import error
 from flask import Flask, request, make_response, render_template, abort
 import sqlite3
 
@@ -10,8 +11,14 @@ try:
     cur.execute("""
         INSERT INTO movie VALUES
             ('Monty Python and the Holy Grail', 1975, 8.2),
-            ('And Now for Something Completely Different', 1971, 7.5),
-            ('saint{if_sqli_is_so_good_wheres_sqli_2?}', 1969, 4.20)
+            ('And Now for Something Completely Different', 1971, 7.5)
+    """)
+    cur.execute("CREATE TABLE users(username, password, ssn)")
+    cur.execute("""
+        INSERT INTO users VALUES
+            ('Jeff Bezos', 'ihateunions', '765652'),
+            ('Michal Morgan', 'shentown', '675477'),
+            ('Admin', 'MicrophoneButtonShingleTree', 'saint{if_sqli_is_so_good_wheres_sqli_2?}')
     """)
 except sqlite3.OperationalError:
     print('table already exists')
@@ -40,9 +47,9 @@ def index():
         cur = con.cursor()
 
         try:
-            res = cur.execute(f"SELECT title FROM movie WHERE title = '{query}'")
-        except sqlite3.OperationalError:
-            return render_template('500.html', query=query), 500
+            res = cur.execute(f"SELECT title, year, score FROM movie where title = '{query}'")
+        except sqlite3.Error as e:
+            return render_template('500.html', query=query, error=e), 500
 
 
         output = res.fetchall()
